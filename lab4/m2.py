84d5573dd56d14e7ec278c4adb2ccdb09dff94e9
Linakbtu <l_kaldanova@kbtu.kz>
2025-02-15 13:16:40 +0500

    main

--- lab4/m2.py
+++ lab4/m2.py
@@ -3,4 +3,4 @@ base1 = float(input("Base, first value: "))
 base2 = float(input("Base, second value: "))
 
 area = 0.5 * (base1 + base2) * height
-print("Expected Output:", area)
+print("Area:", area)
--- lab4/m4.py
+++ lab4/m4.py
@@ -2,4 +2,4 @@ base = float(input("Length of base: "))
 height = float(input("Height of parallelogram: "))
 
 area = base * height
-print("Expected Output:", area)
+print("Area:", area)
