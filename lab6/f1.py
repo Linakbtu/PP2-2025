a714709d0454249c4dc50686bde2b25e314f2639
Linakbtu <l_kaldanova@kbtu.kz>
2025-03-06 14:38:53 +0500

    main

--- lab6/f1.py
+++ lab6/f1.py
@@ -1,6 +1,6 @@
 import os
 
-path = "."  
+path = "sample.txt"  
 
 print("Directories:", [d for d in os.listdir(path) if os.path.isdir(d)])
 print("Files:", [f for f in os.listdir(path) if os.path.isfile(f)])
--- lab6/f6.py
+++ lab6/f6.py
@@ -1,4 +1,4 @@
-for letter in range(65, 91):  # От A (65) до Z (90)
+for letter in range(65, 91):  
     filename = chr(letter) + ".txt"
     with open(filename, "w") as file:
         file.write(f"This is {filename}\n")
